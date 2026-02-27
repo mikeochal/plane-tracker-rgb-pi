e86598a24aa1c12916fd2a4530208d2581795b25
mikeochal <30981196+mikeochal@users.noreply.github.com>
2026-02-27 15:16:44 -0500

    Update config.py

--- its-a-plane-python/config.py
+++ its-a-plane-python/config.py
@@ -1,15 +1,15 @@
 ZONE_HOME = {
-    "tl_y": 37.574200, # Top-Left Latitude (deg) https://www.latlong.net/ or google maps. The bigger the zone, the more planes you'll get. My zone is ~3.5 miles in each direction or 10mi corner to corner. 
-    "tl_x": -122.315938, # Top-Left Longitude (deg)
-    "br_y": 37.556396, # Bottom-Right Latitude (deg)
-    "br_x": -122.167870 # Bottom-Right Longitude (deg)
+    "tl_y": 33.650965, # Top-Left Latitude (deg) https://www.latlong.net/ or google maps. The bigger the zone, the more planes you'll get. My zone is ~3.5 miles in each direction or 10mi corner to corner. 
+    "tl_x": -84.448713, # Top-Left Longitude (deg)
+    "br_y": 33.648697, # Bottom-Right Latitude (deg)
+    "br_x": -84.397622 # Bottom-Right Longitude (deg)
 }
 LOCATION_HOME = [
-    37.548231, # Latitude (deg)
-    -122.277988 # Longitude (deg)
+    33.777871, # Latitude (deg)
+    -84.384149 # Longitude (deg)
 ]
-TEMPERATURE_LOCATION = "37.548231,-122.277988" #same as location home
-TOMORROW_API_KEY = "DkrARHXijK9lUFFaM2degl5Y0ExLueK1" # Get an API key from https://tomorrow.io they only allows 25 pulls an hour, if you reach the limit you'll need to wait until the next hour 
+TEMPERATURE_LOCATION = "33.777871,-84.384149" #same as location home
+TOMORROW_API_KEY = "f8yOJAcsde9zZiWe5krBghRPiRkzPjok" # Get an API key from https://tomorrow.io they only allows 25 pulls an hour, if you reach the limit you'll need to wait until the next hour 
 TEMPERATURE_UNITS = "imperial" #can use "metric" if you want, same for distance 
 DISTANCE_UNITS = "imperial"
 CLOCK_FORMAT = "12hr" #use 12hr or 24hr
@@ -19,8 +19,9 @@ BRIGHTNESS_NIGHT = 50
 NIGHT_BRIGHTNESS = False #True for on False for off
 NIGHT_START = "22:00" #dims screen between these hours
 NIGHT_END = "07:00"
-GPIO_SLOWDOWN = 2 #depends what Pi you have I use 2 for Pi 3 and 1 for Pi Zero
-JOURNEY_CODE_SELECTED = "SFO" #your home airport code
+GPIO_SLOWDOWN = 3 #depends what Pi you have I use 2 for Pi 3 and 1 for Pi Zero
+JOURNEY_CODE_SELECTED = "ATL" #your home airport code
 JOURNEY_BLANK_FILLER = " ? " #what to display if theres no airport code
 HAT_PWM_ENABLED = True #only if you haven't soldered the PWM bridge use True if you did
+
 FORECAST_DAYS = 3 #today plus the next two days
